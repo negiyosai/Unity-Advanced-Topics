1801d3e7a789e4fa01213c665aece5d3f54c247a
vivek_negi99 <vivek99.work@gmail.com>
2019-05-30 14:00:06 +0530

    Added Quaternion Rotation, LookRotation and Slerp scripts.

---
+++ Assets/CanonLookRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonLookRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform sphere;
+
+    void Update()
+    {
+
+        //SnapToRotation();
+        LerpRotation();
+
+    }
+
+    void SnapToRotation() {
+
+        //directoin = destination - source;
+        Vector3 directionToFace = sphere.position - this.gameObject.transform.position; //or simply transform.rotation as it will get the rotation of the object with which the script is attached
+
+        Debug.DrawRay(transform.position, directionToFace, Color.red);  //Debug.DrawRay(source(this gameobject), destination(sphere))
+
+        this.gameObject.transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+
+    void LerpRotation() {
+        //directoin = destination - source;
+        Vector3 directionToFace = sphere.position - this.gameObject.transform.position; //or simply transform.rotation as it will get the rotation of the object with which the script is attached
+
+        Debug.DrawRay(transform.position, directionToFace, Color.red);  //Debug.DrawRay(source(this gameobject), destination(sphere))
+
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+
+        this.gameObject.transform.rotation = Quaternion.Slerp(this.gameObject.transform.rotation, targetRotation, Time.deltaTime);
+                                             
+    }
+
+}
---
+++ Assets/Rotation/SpawnCubeRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubeRotation : MonoBehaviour
+{
+
+    public GameObject Cube;
+  
+    void Start()
+    {
+        Instantiate(Cube, Vector3.zero, Quaternion.Euler(0, 35, 0)); // spawns the cube at an angle of 35 degree on the y axis
+        Instantiate(Cube, new Vector3(-4, 0,0), Quaternion.identity); // spawns with rotation set to 0 in all axis
+        
+    }
+
+    
+}
