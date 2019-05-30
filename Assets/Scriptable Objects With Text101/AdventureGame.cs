426b2a539ff1196656b4db9a56564f7d5d3b7a35
vivek_negi99 <vivek99.work@gmail.com>
2019-05-30 20:43:02 +0530

    Added Scriptable objects

---
+++ Assets/Scriptable Objects With Text101/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]
+    Text gameText;
+    [SerializeField]
+    State startingState; 
+
+    State state;
+
+    void Start()
+    {
+        state = startingState;
+        gameText.text = state.GetStateStory();
+    }
+
+    private void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates(); //this stores the return
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))  //this increases Alpha1 to Alpha 2 and so on
+            {
+
+                state = nextStates[i];  // this will remove the out of bound array error
+
+            }
+
+        }
+
+        gameText.text = state.GetStateStory();
+    }
+
+
+}
---
+++ Assets/Scriptable Objects With Text101/State.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateTEST")] // Now when you right click on the unity project space you will see this option along with c# script, material etc
+public class State : ScriptableObject
+{
+
+    [TextArea(14, 10)] [SerializeField] string storyText;
+
+    [SerializeField]
+    State[] nextStates; //Declaring array of type State 
+
+    public string GetStateStory() {
+
+        return storyText;
+
+    }
+     
+    public State[] GetNextStates()  //array method
+    { 
+
+        return nextStates; // reutrn array to arry method
+    }
+
+
+
+
+
+}
+ 
